b4518848a3adff8e3682303b28e4d5d7f6d990f3
Anishsingh8467 <singhanish05489@gmail.com>
2026-07-27 06:37:53 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"  // treat as all JS cade as newer version
+
+//alter(3 + 3)   // we are using node js not browsers
+
+console.log(3 + 3)
+
+console.log("Anish")
+
+let name = "Anish"
+let age = 22
+let isLoggedIn = false
+let State;
+
+//number => 2 to power 53
+//bigint
+//String => ""
+//boolean => true/false
+//null => standalone value
+//undefined
+//symbol => uniqe
+
+//object 
+
+console.log(typeof "Anish");
+console.log(typeof age);
+console.log(typeof null); // o/p = object
+console.log(typeof undefined); // op = undefined
