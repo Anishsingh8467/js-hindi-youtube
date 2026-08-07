d4268c788ca6037047783e07a068cbe00e873802
Anishsingh8467 <singhanish05489@gmail.com>
2026-08-07 05:40:36 +0000

    arrows

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,56 @@
+const user = {
+    username: "hitesh",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "hitesh"        /// function ke andar mai this nahi use kar pa raha hu ye kewal object ke andar hi proper use ho pa raha hai
+//     console.log(this.username);
+// }
+
+// chai()
+
+//**** out put (undifined)
+
+// const chai = function () {
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+const chai =  () => {     //   this is arrow function
+    let username = "hitesh"
+    console.log(this);
+}
+
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2
+
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+const addTwo = (num1, num2) => ({username: "hitesh"})
+
+
+console.log(addTwo(3, 4))
+
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
